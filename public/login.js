48f95b29af99b585e02a546900878f21d782f78b
2313Bernice <b2029bvlorenzo@pshs.edu.ph>
2026-02-17 12:05:25 +0800

    project update 1.0

---
+++ public/login.js
@@ -0,0 +1,12 @@
+function inStyle(ele){
+    console.log(ele);
+    ele.style.backgroundColor = "rgb(37, 111, 80)";
+    ele.style.color="white";
+}
+
+function outStyle(ele){
+    console.log(ele);
+    ele.style.backgroundColor = "rgb(242, 242, 255)";
+     ele.style.color="black";
+    
+}
